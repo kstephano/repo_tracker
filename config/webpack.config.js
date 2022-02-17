34ab4c35f412ee680961edd9083eac56b2d2ffc9
Kelvin <kstephano@gmail.com>
2022-02-17 12:14:21 +0000

    changed target build directory to dist

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,7 +8,7 @@ const config = {
   entry: [path.resolve(ROOT_DIRECTORY, "src/index.js")], // the main JavaScript file of the project
   output: {
     // instructions for compiling the code
-    path: path.resolve(ROOT_DIRECTORY, "src"), // the file where the compiled code should go
+    path: path.resolve(ROOT_DIRECTORY, "dist"), // the file where the compiled code should go
     filename: "bundle.js", // the file name of the compiled code
     publicPath: "/", // specifies the base path for all the assets within your application.
   },
